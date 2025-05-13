478535ee6d0e2fab64fdb6dc30fa022037110d9a
Tanay Singh <tanayparihar@gmail.com>
2025-05-13 23:53:59 +0530

    Solved Permutations

---
+++ array.java
@@ -0,0 +1,27 @@
+class Arrays
+{
+    public static void main(String arp[])
+    {
+        int arr[]={1,2,3,4,5};
+        System.out.println(arr[0]);
+        System.out.println(arr);
+    }
+}
+
+class abc
+{
+    public static void main(String[] args) {
+        int i;
+        String st="Tanay";
+        // System.out.println(st);
+        char ch;
+        // for(i=st.length()-1;i>=0  ;i--) // i= 4,3, 2
+        // {
+        //     ch=st.charAt(i); // y, a , n
+        //     stnew=stnew+ ch; // stnew= y, ya, yan
+        // }
+        // System.out.println(stnew); 
+       st=st.replace('a','i' );
+        System.out.println(st);
+    }
+}
---
+++ permutations.java
@@ -0,0 +1,27 @@
+class Solution {
+    public List<List<Integer>> permute(int[] nums) {
+        List<List<Integer>> list=new ArrayList<>();
+        List<Integer> slist=new ArrayList<>();
+        permutehelper(nums, list, slist);
+        return list;
+    }
+    private void permutehelper(int[] nums, List<List<Integer>> list, List<Integer> slist) {
+        if(slist.size()==nums.length)
+        {
+        list.add(new ArrayList<>(slist));
+        return ;
+        }
+
+        for(int i=0;i<nums.length;i++)
+        {
+            if(slist.contains(nums[i]))
+            {
+                continue;
+            }
+            slist.add(nums[i]);
+            permutehelper(nums, list, slist);
+            slist.remove(slist.size()-1);
+        }   
+        // return list;     
+    }
+}
